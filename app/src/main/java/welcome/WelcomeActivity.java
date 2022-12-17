b918de3ded001ea153e2448c98ad029d4c81b3ae
541056967@qq.com <541056967@qq.com>
2022-12-17 19:45:42 +0800

    解决启动页问题

---
+++ app/src/main/java/welcome/WelcomeActivity.java
@@ -0,0 +1,124 @@
+package welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.wonderland.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import views.HomeActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private ViewPager vp;
+    private Button btn_start;
+    private List<Fragment> fragments;
+    private ImageView iv1, iv2, iv3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
+        setContentView(R.layout.activity_welcome);
+        initData();
+        initViews();
+        setListener();
+    }
+
+    public void initData() {
+        fragments = new ArrayList<>();
+        fragments.add(new WelcomeFragment(R.drawable.test));
+        fragments.add(new WelcomeFragment(R.drawable.test));
+        fragments.add(new WelcomeFragment(R.drawable.test));
+    }
+
+    public void initViews() {
+        vp = findViewById(R.id.vp_welcome);
+        btn_start = findViewById(R.id.btn_start);
+        iv1 = findViewById(R.id.iv1);
+        iv2 = findViewById(R.id.iv2);
+        iv3 = findViewById(R.id.iv3);
+        vp.setOffscreenPageLimit(3);
+        vp.setAdapter(new MyPageAdapter(getSupportFragmentManager()));
+        vp.addOnPageChangeListener(new MyPageChangeListener());
+    }
+
+    public void setListener() {
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * ViewPager适配器
+     */
+    private class MyPageAdapter extends FragmentPagerAdapter {
+
+
+        public MyPageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+    }
+
+    /**
+     * ViewPager滑动页面监听器
+     */
+    private class MyPageChangeListener implements ViewPager.OnPageChangeListener {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        /**
+         * 根据页面不同动态改变红点和在最后一页显示立即体验按钮
+         *
+         * @param position
+         */
+        @Override
+        public void onPageSelected(int position) {
+            btn_start.setVisibility(View.GONE);
+            iv1.setImageResource(R.drawable.spot2);
+            iv2.setImageResource(R.drawable.spot2);
+            iv3.setImageResource(R.drawable.spot2);
+            if (position == 0) {
+                iv1.setImageResource(R.drawable.spot1);
+            } else if (position == 1) {
+                iv2.setImageResource(R.drawable.spot1);
+            } else {
+                iv3.setImageResource(R.drawable.spot1);
+                btn_start.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    }
+}
---
+++ app/src/main/java/welcome/WelcomeFragment.java
@@ -0,0 +1,35 @@
+package welcome;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.example.wonderland.R;
+
+public class WelcomeFragment extends Fragment {
+
+    private ImageView imageView;
+    private int drawableResId;
+
+    public WelcomeFragment(int drawableResId) {
+        this.drawableResId = drawableResId;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_welcome_view, container, false);
+        imageView = view.findViewById(R.id.iv_background);
+        Glide.with(view).load(drawableResId).into(imageView);
+        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        return view;
+    }
+
+}
